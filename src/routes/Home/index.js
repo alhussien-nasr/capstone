4705933735c33d5bafb24a99392d37f6c6fc70df
hussein <alhussien.n.g@gmail.com>
2022-12-10 15:47:54 +0200

    clean home/index

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -1,6 +1,4 @@
 import React from "react";
-import CategoryCard from "../../components/CategoryCard";
-import { category } from "../../Data/category";
 import "./styles.css";
 import CaregoryList from "../../components/CategoryList";
 const Home = () => {
