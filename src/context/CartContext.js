02a005e165ccd5a163b00b14b1ab8a3b818e2ab3
hussein <alhussien.n.g@gmail.com>
2022-12-12 16:31:11 +0200

    remove setState

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -34,12 +34,7 @@ const removeCartItem = (cartItems, cartItemToRemove) => {
 
 export const CartContext = createContext({
   dropdown: false,
-  setDropdown: () => {},
   cartItems: [],
-  addItemToCart: () => {},
-  removeItemfromCart: () => {},
-  clearItem: () => {},
-  toggleDropdown: () => {},
   cartCount: 0,
   cartTotal: 0,
 });
